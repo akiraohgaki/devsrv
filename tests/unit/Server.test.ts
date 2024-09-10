f7d73d111ae6513ad94c3159db2a2fd22f76417c
Akira Ohgaki <akiraohgaki@gmail.com>
2024-09-11 04:49:12 +0900

    Add test code

---
+++ tests/unit/Server.test.ts
@@ -0,0 +1,121 @@
+import { assertStrictEquals } from '@std/assert';
+
+import { Server } from '../../mod.ts';
+
+async function wait(ms: number): Promise<void> {
+  await new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+Deno.test('Server', async (t) => {
+  await t.step('start and stop', async () => {
+    const server = new Server();
+
+    let isRunning = false;
+
+    server.start();
+    await wait(100);
+    try {
+      server.start();
+    } catch (exception) {
+      if (exception instanceof Error) {
+        console.log(exception.message);
+        isRunning = exception.message.search('already running') !== -1;
+      }
+    }
+
+    assertStrictEquals(isRunning, true);
+
+    server.stop();
+    await wait(100);
+    try {
+      server.stop();
+    } catch (exception) {
+      if (exception instanceof Error) {
+        console.log(exception.message);
+        isRunning = exception.message.search('not running') === -1;
+      }
+    }
+
+    assertStrictEquals(isRunning, false);
+  });
+
+  await t.step('directory index', async () => {
+    const server = new Server({
+      hostname: 'localhost',
+      port: 3333,
+      documentRoot: './tests/demo',
+      directoryIndex: 'index.html',
+    });
+
+    server.start();
+    await wait(100);
+
+    const responseA = await fetch('http://localhost:3333/');
+    const contentA = await responseA.text();
+
+    const responseB = await fetch('http://localhost:3333/index.html');
+    const contentB = await responseB.text();
+
+    const responseC = await fetch('http://localhost:3333/dummy');
+    const contentC = await responseC.text();
+
+    server.stop();
+    await wait(100);
+
+    assertStrictEquals(responseA.status, 200);
+    assertStrictEquals(responseB.status, 200);
+    assertStrictEquals(responseB.status, 200);
+    assertStrictEquals(contentA, contentB);
+    assertStrictEquals(contentA, contentC);
+  });
+
+  await t.step('bundle', async () => {
+    const server = new Server({
+      hostname: 'localhost',
+      port: 3333,
+      documentRoot: './tests/demo',
+      bundle: true,
+    });
+
+    server.start();
+    await wait(100);
+
+    const response = await fetch('http://localhost:3333/main.bundle.js');
+    const content = await response.text();
+
+    server.stop();
+    await wait(100);
+
+    assertStrictEquals(response.status, 200);
+    assertStrictEquals(content.search('function __default') !== -1, true);
+  });
+
+  await t.step('not found', async () => {
+    const server = new Server({
+      hostname: 'localhost',
+      port: 3333,
+      documentRoot: './tests/demo',
+      directoryIndex: 'dummy.html',
+      bundle: true,
+    });
+
+    server.start();
+    await wait(100);
+
+    const responseA = await fetch('http://localhost:3333/dummy.html');
+    const contentA = await responseA.text();
+    console.log(contentA);
+
+    const responseB = await fetch('http://localhost:3333/dummy.bundle.js');
+    const contentB = await responseB.text();
+    console.log(contentB);
+
+    server.stop();
+    await wait(100);
+
+    assertStrictEquals(responseA.status, 404);
+    assertStrictEquals(responseB.status, 404);
+  });
+});
