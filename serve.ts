7b985379de9266dc13dbe6c638e800feb8d3775b
Akira Ohgaki <akiraohgaki@gmail.com>
2024-09-15 01:08:56 +0900

    Rename server.ts to serve.ts

---
+++ serve.ts
@@ -0,0 +1,16 @@
+/**
+ * @module @akiraohgaki/devsrv/serve
+ */
+
+import { Server, serverOptions } from './mod.ts';
+import { args, booleanValue, numberValue, stringValue } from './src/cliUtil.ts';
+
+const server = new Server({
+  hostname: stringValue(args.h ?? args.host, serverOptions.hostname),
+  port: numberValue(args.p ?? args.port, serverOptions.port),
+  directoryIndex: stringValue(args['directory-index'], serverOptions.directoryIndex),
+  bundle: booleanValue(args.bundle, serverOptions.bundle),
+  documentRoot: stringValue(args._[0], serverOptions.documentRoot),
+});
+
+server.start();
