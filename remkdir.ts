c2bdc79d3861aa592ac011e51886a35ec5c7c188
Akira Ohgaki <akiraohgaki@gmail.com>
2024-09-15 02:08:16 +0900

    Fix remkdir.ts

--- remkdir.ts
+++ remkdir.ts
@@ -6,15 +6,20 @@ import $ from '@david/dax';
 
 import { args, stringValue } from './src/cliUtil.ts';
 
+const includes = stringValue(args.includes, '');
 const targetDirectory = stringValue(args._[0], '');
-const includes = stringValue(args._[1], '');
 
 if (targetDirectory) {
+  console.log(`includes: ${includes}`);
+  console.log(`targetDirectory: ${targetDirectory}`);
+
   await $`rm -rf ${targetDirectory}`;
   await $`mkdir -p ${targetDirectory}`;
 
   if (includes) {
-    await $`cp -a ${includes} ${targetDirectory}`;
+    for (const include of includes.split(',')) {
+      await $`cp -r ${include.trim()} ${targetDirectory}`;
+    }
   }
 
   await $`ls -a ${targetDirectory}`;
