2508bce16ca78b1f7da7ded9e4787e1a050d0d15
Akira Ohgaki <akiraohgaki@gmail.com>
2024-11-13 16:20:38 +0000

    Fix playground page.

--- src/playground-page.ts
+++ src/playground-page.ts
@@ -239,11 +239,15 @@ code.set(codeContent);
  */
 const playgroundPage: string = `
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="format-detection" content="telephone=no, email=no, address=no">
+
   <title>Playground</title>
+
   <style>${style}</style>
 </head>
 
