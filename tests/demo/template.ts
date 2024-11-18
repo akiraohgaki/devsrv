b2ed1ee4dbe3f081a39e0e06ebd344540664db6a
Akira Ohgaki <akiraohgaki@gmail.com>
2024-11-18 14:10:00 +0000

    Update demo

--- tests/demo/template.ts
+++ tests/demo/template.ts
@@ -1,9 +1,8 @@
 const template = document.createElement('template');
 
 template.innerHTML = `
-  <p>
-  <a href="/main.ts">main.ts</a> was bundled into <a href="/main.bundle.js">main.bundle.js</a>.
-  </p>
+  <p><a href="/main.ts">main.ts</a> was bundled into <a href="/main.bundle.js">main.bundle.js</a>.</p>
+  <p>Also try <a href=".playground">Code playground</a>.</p>
 `;
 
 export default template;
