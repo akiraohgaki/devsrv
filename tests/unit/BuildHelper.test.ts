a5c86915768df7e1efdb045e1bd98d6109264eda
Akira Ohgaki <akiraohgaki@gmail.com>
2024-11-19 01:18:02 +0000

    Update test code

--- tests/unit/BuildHelper.test.ts
+++ tests/unit/BuildHelper.test.ts
@@ -15,16 +15,24 @@ Deno.test('BuildHelper', async (t) => {
     await buildHelper.bundleFile(
       'tests/demo/main.ts',
       'tmp/demo/main.bundle.js',
+    );
+    await buildHelper.bundleFile(
+      'tests/demo/main.ts',
+      'tmp/demo/main.bundle.min.js',
       { minify: true },
     );
 
     assertStrictEquals((await Deno.stat('tmp/demo/main.bundle.js')).isFile, true);
+    assertStrictEquals((await Deno.stat('tmp/demo/main.bundle.min.js')).isFile, true);
   });
 
   await t.step('bundle', async () => {
     const buildHelper = new BuildHelper();
-    const code = await buildHelper.bundle('tests/demo/main.ts', { minify: true });
+    const code = await buildHelper.bundle('tests/demo/main.ts');
+    const codeMin = await buildHelper.bundle('tests/demo/main.ts', { minify: true });
 
+    assertStrictEquals(code !== codeMin, true);
     assertStrictEquals(await Deno.readTextFile('tmp/demo/main.bundle.js'), code);
+    assertStrictEquals(await Deno.readTextFile('tmp/demo/main.bundle.min.js'), codeMin);
   });
 });
