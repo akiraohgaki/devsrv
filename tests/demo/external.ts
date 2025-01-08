8afe7b9f49fe12024cb77a1603410cf79bbc769d
Akira Ohgaki <akiraohgaki@gmail.com>
2025-01-09 02:34:19 +0900

    Update demo

---
+++ tests/demo/external.ts
@@ -0,0 +1,5 @@
+import '@scope/package';
+import 'jsr:@scope/package@1.0.0';
+import 'npm:@scope/package@1.0.0';
+import 'https://hostname/@scope/package@1.0.0';
+import './node_modules/@scope/package/index.js';
